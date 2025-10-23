2746dd957d507f6b8afb28e0cba4ddbda514f1e9
Ifrim Alexandra <157494822+ale23yfm@users.noreply.github.com>
2025-10-23 19:12:18 +0300

    Update lab4.asm

--- lab4.asm
+++ lab4.asm
@@ -14,6 +14,7 @@ segment  code use32 class=code ; segmentul de cod
 start: 
     ;build the word c in BX
     mov BX, 0 ;BX = 00000000 00000000
+
         ;the bits 0-2 of c are the same as the bits 10-12 of b
     mov AX, [a]
     ;or mov BX, 0001_1100_0000_0000b and the and AX, BX
@@ -24,16 +25,20 @@ start:
     shr AX, CL 
     ;concatenation
     or BX, AX
+
         ;the bits 3-6 of c have the same value 1
     or BX, 0000_0000_0111_1000b
+
         ;the bits 7-10 of c are the same bits 1-4 of a
     mov DX, [a]
     and DX, 0000_0000_0001_1110b
     shl DX, 6
     or BX, DX
+
         ;the bits 11-12 have the value 0
     ; in bits mask 0 for and, 1 for or
     and bx, 1110_0111_1111_1111b
+
         ;the bits 13-15 of c are the invert of the bits 9-11 of b
     mov AX, [b]
     not AX
