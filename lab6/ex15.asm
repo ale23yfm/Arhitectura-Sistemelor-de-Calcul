b2b90e21253912264099c309e373886a3a87d8fe
Ifrim Alexandra <157494822+ale23yfm@users.noreply.github.com>
2025-11-13 19:53:02 +0200

    Add assembly code for sorting double words
    Implement sorting of double words in descending order.

---
+++ lab6/ex15.asm
@@ -0,0 +1,64 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+segment data use32 class=data
+    ; ...    
+    s dd 12345607h, 1A2B3C15h
+    l equ $-s
+    d times l db 0
+       
+; our code starts here
+segment code use32 class=code
+        ;15. An S string of double words is given.
+        ;Obtain the string D consisting of the bytes of the double words in the string D sorted in descending order in the unsigned interpretation.
+            ;Example:
+            ;s DD 12345607h, 1A2B3C15h
+            ;d DB 56h, 3Ch, 34h, 2Bh, 1Ah, 15h, 12h, 07h
+    start:
+        mov ECX, l
+        JECXZ end_loop
+        mov ESI, s
+        mov EDI, d
+        CLD ; clear DF flag
+        
+        start_loop:
+            mov BX, 0 
+            lodsb
+            stosb
+            lodsb
+            stosb
+            lodsb
+            stosb
+            lodsb
+            stosb
+                       
+        loop start_loop  
+
+        mov ecx, 7          
+        sort:
+                mov edi, 8
+                mov ebx, 7
+            sort_term:
+                mov al, [edi]
+                mov dl, [edi+1]
+                cmp al, dl
+                jge no_swap
+                    mov [edi], dl
+                    mov [edi+1], al
+            no_swap:
+                inc edi
+                dec ebx
+                jnz sort_term
+        loop sort
+        
+        end_loop:
+            ; exit(0)
+            push    dword 0      ; push the parameter for exit onto the stack
+            call    [exit]       ; call exit to terminate the program
